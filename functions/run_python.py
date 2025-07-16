c223daa215d13396d32720851fe7f2f28c3f94ea
belsonc <craig.belson@gmail.com>
2025-07-15 23:31:18 -0400

    R: finally got back to writing some code, not done with the next lesson but at least I started

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,16 +1,27 @@
 #run_python.py
+import os
 
-def run_python_file(working_directory, file_path):
+def run_python_file(working_directory, file_path, args=[]):
+    
 
     #if the file_path is outside of the working directory, return a string with an error:
     #f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    try:
+        working_directory_abspath = os.path.abspath(working_directory)
+        file_path_abspath = os.path.abspath(os.path.join(working_directory, file_path))
+
+        if not file_path_abspath.startswith(working_directory_abspath):
+            return (f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory')
 
     #if the file_path doesn't exist, return an error string:
     #f'Error: File "{file_path}" not found.'
+        if not os.path.exists(file_path_abspath):
+            return (f'Error: "{file_path}" not found.')
 
     #if the file doesn't end in .py, return an error string:
     #f'Error: "{file_path}" is not a Python file.'
-
+        if not file_path_abspath.endswith('.py'):
+            return (f'Error: "{file_path}" is not a Python file.')
     #use subprocess.run function to execute the python file
     #-set a timeout of 30 seconds to prevent infinite execution
     #-capture both stdout and stderr
